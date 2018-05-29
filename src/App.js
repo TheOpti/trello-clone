6ce30afdca74f28b69e00fd89ddd9177dca26556
TheOpti <wieckowski.piotr.93@gmail.com>
2018-05-29 11:16:18 +0200

    Add bootstrap@v.4

--- src/App.js
+++ src/App.js
@@ -13,6 +13,15 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
